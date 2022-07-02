47eb5a68f813ade47c8b4d97ab3d7e860a704553
Alexey Osipenko <78563246+Aleospen@users.noreply.github.com>
2022-07-02 18:53:08 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
 let Name = document.getElementById("name");
 let Author = document.getElementById("author");
-let Pages = document.getElementById("pages");
+ let Pages = document.getElementById("pages");
 let Title = document.getElementById("title");
+
+let nameBook = Name.value
+let AuthorValueInput = Author.value;
+let PagesValueInput = Pages.value 
 
 let numbs = 0;
 console.log(numbs);
@@ -14,7 +18,6 @@ function Book(Name, Author, Pages, Title) {
     this.Author = Author.value;
     this.Pages = Pages.value;
     this.Title = Title.value;
-    
 }
 
 function check() {
@@ -49,33 +52,101 @@ function displayBook () {
     first_div.appendChild(divSecundus)
     // divSecundus
 
-
-    numbs2 = numbs;
     console.log(numbs);
 
 
-
-    let imgRead =  document.createElement('img')
+    let imgRead = document.createElement('img')
     imgRead.classList.add('img_read')
     imgRead.src = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_212516.png&f=1&nofb=1";
     let points = document.getElementsByClassName('point')
-    points[numbs2].appendChild(imgRead) 
+    points[numbs].appendChild(imgRead) 
 
-
+    
 
         //let div3 = document.createElement('div');
         //div3.classList.add('imgDIV')
-        
+                                                                                        // COVER
         //let imageOfDIVIII = document.createElement('imgDIV');
-        let imageOfDIVIII = document.createElement('img');
-        imageOfDIVIII.classList.add('imgLogo');
-        imageOfDIVIII.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.8LPxqTvPFvkgPax6lRcYPwHaKe%26pid%3DApi&f=1'
-        
-        //let block3 = document.getElementsByClassName('imgDIV')
-        //block3[numbs2].appendChild(imageOfDIVIII);
-        points[numbs2].appendChild(imageOfDIVIII)   
 
-    
+        let coverSRC = Title.value
+        console.log(coverSRC);
+
+
+        let cover = document.createElement('img');
+        cover.classList.add('imgLogo');
+        cover.src = coverSRC
+       
+        
+        points[numbs].appendChild(cover)   
+
+
+
+                                                                        // Name of Book
+        // let NameBlock = document.createElement('div')    // 1                                                           
+         let stringName = document.createElement('p');
+         stringName.classList.add('Name')   
+          
+
+         //  stringName.insertAdjacentText('Book');
+        //    document.getElementsByClassName('Name').innerHTML = 'text'
+         
+        let nameVAR = document.createTextNode(nameBook);
+
+        document.getElementsByClassName('Name').innerHTML = nameVAR;    
+
+        
+        points[numbs].appendChild(nameVAR)  
+
+
+        let stringVoid = document.createElement('p');  
+
+        stringVoid.classList.add('Void') 
+
+        let textVal = document.createTextNode('     ');
+
+        document.getElementsByClassName('Void').innerHTML = '        ';    
+   
+        points[numbs].appendChild(textVal)
+
+
+
+
+        // Author
+         let stringAuthor = document.createElement('p');
+         stringName.classList.add('Author')  
+        
+         let AuthorVAR = document.createTextNode(AuthorValueInput);
+
+         document.getElementsByClassName('Author').innerHTML = AuthorVAR;    
+         
+         points[numbs].appendChild(AuthorVAR)
+ 
+
+         let Void = document.createElement('p');  
+
+        stringVoid.classList.add('Void') 
+
+        let text = document.createTextNode('     ');
+
+        document.getElementsByClassName('Void').innerHTML = '    ';    
+   
+        points[numbs].appendChild(text)
+
+
+          
+
+        // Pages
+        let stringPages = document.createElement('p');
+        
+        stringPages.classList.add('Pages')  
+  
+        let PagesVAR = document.createTextNode(PagesValueInput);
+
+        document.getElementsByClassName('Pages').innerHTML = PagesVAR;    
+   
+        points[numbs].appendChild(PagesVAR)
+
+        
 
     console.log(numbs);
  
@@ -85,6 +156,10 @@ function displayBook () {
 }
 
 console.log(numbs);
+
+
+    
+
 
 // const container = document.createElement('div');
 // container.classList.add("DataChest")    
